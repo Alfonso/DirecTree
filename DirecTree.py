f384d0bba14f9813ff118fd052fadf9260819c5d
alfonso <highestlevel99yahoo.com>
2019-06-24 11:36:45 -0400

    Add directory check

--- DirecTree.py
+++ DirecTree.py
@@ -6,23 +6,18 @@ import sys
 def getDirName(dirPath):
     return dirPath.split('\\')[len(dirPath.split('\\')) - 1]
 
-
-
 # make sure that they have the correct number of arguments
 if(len(sys.argv) != 2):
     print("Please input the correct number of arguments")
-    exit();
+    exit()
 
-# the second argument should be the path of the start directory
-# can be either the complete path or a relative path
-
-#Check ^^
+if not os.path.isdir(sys.argv[1]):
+    print("Please input a correct path to a directory")
+    exit()
 
 count = 0
 first = 0
-
 rootDir = sys.argv[1]
-
 startSlash = rootDir.count('\\')
 
 for dirName,subdirList, fileList,in os.walk(rootDir):
